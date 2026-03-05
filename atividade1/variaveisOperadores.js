5adaca6f237472e2c88ada85fb37fcb7265ad0d2
ValoisJoao <joaovictorvalois@gmail.com>
2026-03-05 13:28:05 -0300

    Atividade 1 - Variáveis e tipos primitivos
    Aqui está a primeira atividade da minha aula de programação para web, atividade sobre variáveis e tipos primitivos

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+let num_A = 10;
+let num_B = 5;
+
+// Soma
+let resultadoSoma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${resultadoSoma}`);
+
+// Subtração
+let resultadoSub = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${resultadoSub}`);
+
+// Multiplicação
+let resultadoMult = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${resultadoMult}`);
+
+// Divisão
+let resultadoDiv = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${resultadoDiv}`);
