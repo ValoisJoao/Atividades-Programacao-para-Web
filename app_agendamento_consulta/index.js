82f78ba2de9b2c8105b75db7265a2c4477e5b168
JOÃO VICTOR VALOIS SILVA <joao.valois@a.ucb.br>
2026-04-27 20:14:10 -0300

    Atualizações

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -3,6 +3,14 @@ const mustacheExpress = require('mustache-express')
 const PORT = 8080;
 const app = express();
 
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/view');
+
+app.get('/', (req, res)=>{
+    res.render("index.html");
+});
+
 app.listen(PORT,()=>{
     console.log('app rodando na porta ' + PORT);
 });
